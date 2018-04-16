8057f33ad63466b1cd1ec9da764c89e4b96a113d
stloder <scl963@gmail.com>
2018-04-16 16:57:37 -0400

    added api route

--- server/api/instruments.js
+++ server/api/instruments.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
 const { Instrument } = require('../db/models');
 
-module.exports = router;
 
 router.get('/', (req, res, next) => {
   Instrument.findAll({})
-    .then( instrument => {
-      if (instrument){
-        res.status(200).json(instrument);
-      } else {
-        res.sendStatus(404);
-      }
-    })
-    .catch(next);
+  .then( instruments => {
+    if (instruments){
+      res.status(200).json(instruments);
+    } else {
+      res.sendStatus(404);
+    }
+  })
+  .catch(next);
 })
+
+
+
+module.exports = router;
