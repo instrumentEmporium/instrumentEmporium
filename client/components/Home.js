358bba510a4d77d19cf28e535d2d0a66d8d053bc
stloder <scl963@gmail.com>
2018-04-20 14:58:55 -0400

    Added background image and start of carousel to home

--- client/components/Home.js
+++ client/components/Home.js
@@ -16,7 +16,19 @@ export default class Home extends React.Component {
     const { topFive } = this.props;
     return (
       <div >
-        <Container
+        <section id="carousel">
+          <div id="carousel-text">
+            <h1>Welcome to the Instrument Emporium!</h1>
+            <Button as={Link} to="/instruments" color="yellow" size="huge">
+              Explore our products
+              <Icon name='right arrow' />
+            </Button>
+          </div>
+          <img className="carousel-image"
+            src="https://upload.wikimedia.org/wikipedia/commons/8/87/Steinway_grand_piano_strings_and_keys.JPG" />
+          <img className="carousel-image hidden" src="http://www.schantzorgan.com/data1/images/schantzorganslide02.jpg" />
+        </section>
+        {/* <Container
           className='homeLanding'
           style={{
             marginBottom: '100px',
@@ -34,11 +46,7 @@ export default class Home extends React.Component {
             }}
           >
           </Header>
-          <Button as={Link} to="/instruments" color="yellow" size="huge">
-            Explore our products
-          <Icon name='right arrow' />
-          </Button>
-        </Container>
+        </Container> */}
         <Grid textAlign='center'>
           {
             topFive && topFive.map(instrument => {
