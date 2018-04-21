2279e638ddedcd8b285b268fed7dd1b6e099bb4d
stloder <scl963@gmail.com>
2018-04-21 15:28:47 -0400

    Added image carousel to home page and split into new component

--- client/components/Home.js
+++ client/components/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 // import SingleInstrument from './SingleInstrument';
 import { Icon, Button, Container, Header, Image, Grid } from 'semantic-ui-react';
 import Instrument from './Instrument';
+import Carousel from './ImageCarousel';
 
 
 export default class Home extends React.Component {
@@ -21,22 +22,9 @@ export default class Home extends React.Component {
 
   render() {
     const { topFive } = this.props;
-    let index=this.state.index;
-    console.log(index)
     return (
       <div >
-        <section id="carousel">
-          <div id="carousel-text">
-            <h1>Welcome to the Instrument Emporium!</h1>
-            <Button as={Link} to="/instruments" color="yellow" size="huge">
-              Explore our products
-              <Icon name='right arrow' />
-            </Button>
-          </div>
-          <img className='carousel-image' src="/images/background" />
-          <img className={`carousel-image ${(index%3 === 2) ? '' : 'hidden'}`} src="/images/background2" />
-          <img className={`carousel-image ${(index%3 === 0) ? '' : 'hidden'}`} src='/images/background3' />
-        </section>
+        <Carousel />
         <Grid textAlign='center'>
           <Grid.Row>
             <h1>Top Rated Products</h1>
