29e7c9535f4afe6a5fe8440233a54ceee5b02ba7
Jared E Jackson <jaredjxn15@gmail.com>
2018-04-24 12:00:23 -0400

    added admin check for delete instrument by id route, and for all users api route

--- server/api/users.js
+++ server/api/users.js
@@ -1,13 +1,10 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
-
+const isAdmin = require('./authMiddleware');
+ 
 module.exports = router
 
-router.get('/', (req, res, next) => {
-    if(req.user){
-      if(req.user.dataValues.admin) next()
-    }
-  }, (req, res, next) => {
+router.get('/', isAdmin, (req, res, next) => {
   User.findAll({
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
@@ -53,13 +50,3 @@ User.findById(req.params.id)
    })
    .catch(next);
   });
-
-  
-  // (req, res, next) => {
-  //   if (req.user) {
-  //     if (req.user.dataValues.admin) next();
-  //   }
-  //   let err = new Error('You\'re not an admin! Get out of here!')
-  //   err.status = 401;
-  //   next(err); //or throw(err);
-  // }
