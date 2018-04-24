cdf679c1f8547a9a1d675b4d7a1b11c20b67d582
Jared E Jackson <jaredjxn15@gmail.com>
2018-04-24 11:31:39 -0400

    fixed merge

--- server/api/users.js
+++ server/api/users.js
@@ -4,6 +4,10 @@ const {User} = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
+    if(req.user){
+      if(req.user.dataValues.admin) next()
+    }
+  }, (req, res, next) => {
   User.findAll({
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
@@ -49,3 +53,13 @@ User.findById(req.params.id)
    })
    .catch(next);
   });
+
+  
+  // (req, res, next) => {
+  //   if (req.user) {
+  //     if (req.user.dataValues.admin) next();
+  //   }
+  //   let err = new Error('You\'re not an admin! Get out of here!')
+  //   err.status = 401;
+  //   next(err); //or throw(err);
+  // }
